cda66207acc218a59a3346cfebd53107d4c17707
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-12-06 13:24:02 +0530

    checked rel attr with noopener & noreferrer values

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
-   <NuxtPage/>
+    <nav>
+      <NuxtLink to="/">Home</NuxtLink> &nbsp;
+      <NuxtLink to="/movies">Movies</NuxtLink> &nbsp;
+      <NuxtLink to="https://vueschool.io">Vue School</NuxtLink>
+    </nav>
+    <NuxtPage/>
   </div>
 </template>
