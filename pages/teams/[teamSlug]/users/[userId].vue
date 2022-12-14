6449b4f87ab2e28045503bfcb84aa1fd7048b9f2
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-12-14 16:02:21 +0530

    Dynamic Route Params -> used useState and computed

--- pages/teams/[teamSlug]/users/[userId].vue
+++ pages/teams/[teamSlug]/users/[userId].vue
@@ -1,14 +1,14 @@
+<script setup>
+const route = useRoute();
+const message = computed(() => `I'm on team ${route.params.teamSlug} and my user id is ${route.params.userId}`)
+</script>
+
+
 <template>
   <div>
-    <p>I'm on team {{ $route.params.teamSlug }} and my user id is {{ $route.params.userId }}</p>
+    <p>{{ message }}</p>
   </div>
 </template>
-
-<script>
-export default {
-  name: "[id]"
-}
-</script>
 
 <style scoped>
 
